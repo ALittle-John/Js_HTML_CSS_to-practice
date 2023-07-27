de2287f7ba75a326c98a7149dadfd4c63411f516
ALittle-John <joaopsousaqi@gmail.com>
2023-07-27 19:33:28 -0300

    feat: new simple project

--- Dark-Mode/js/list.js
+++ Dark-Mode/js/list.js
@@ -1,15 +1,38 @@
+//Primeiro jeito:
 document.addEventListener('DOMContentLoaded', function() {
-  const DarkMode = document.querySelector('.Dark')
-  const LightMode = document.querySelector('.Light')
+  const btn = document.getElementById('DarkMode')
 
-  function DarkModeActive() {
+  btn.addEventListener('change', (e) => {
+    document.body.classList.toggle('dark', e.target.checked)
+  })
+});
 
-  }
+// O ouvinte change é usado para monitorar a mudança de estado do elemento do tipo checkbox.
+// A partir o (e), é o mesmo que adicionar uma função, o qual diz que: esta função anônima com o parâmetro event vai acessar a tag <body> e suas classes CSS. o "e.target.checked" é a variável responsável por verificar se o checkbox está marcado ou não... Ao ser marcado, o método toggle é responsável pela troca / por alterar a tag <body> adcionando ou removendo a classe dark.
+// É necessário  parâmetro Event!
 
-  function LightModeActive() {
+//------------------------------------------------------
 
-  }
-  
-  DarkMode.addEventListener('fullscreenchange', DarkModeActive)
-  LightMode.addEventListener('fullscreenchange', LightModeActive)
-})
+//Segundo jeito:
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   const btn = document.getElementById('DarkMode')
+
+//   btn.addEventListener('change', () => {
+//     document.body.classList.toggle('dark', btn.checked)
+//   })
+// });
+
+// Neste caso o (e) e o e.target não precisam ser referênciados.
+
+//------------------------------------------------------
+
+//Terceiro jeito:
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   const btn = document.getElementById('DarkMode')
+
+//   btn.onchange = function(e) {
+//     document.body.classList.toggle('dark', e.target.checked);
+//   }
+// })
