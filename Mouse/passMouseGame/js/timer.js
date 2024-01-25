da280841305f4fa18502677abcadace51e0c2c31
ALittle-John <joaopsousaqi@gmail.com>
2024-01-25 12:15:25 -0300

    refactor: remove comments, update readme and reorganization

--- Mouse/passMouseGame/js/timer.js
+++ Mouse/passMouseGame/js/timer.js
@@ -1,38 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-  const squadAgain = document.querySelector('.Box')
-  const countdown = document.querySelector('.Inumber')
-  let seconds = 30
-  let TimeInterval
+document.addEventListener("DOMContentLoaded", function () {
+  const squadAgain = document.querySelector(".Box");
+  const countdown = document.querySelector(".Inumber");
+  let seconds = 30;
+  let TimeInterval;
 
   function UpdadeCountdown() {
     seconds--;
-    countdown.textContent = seconds
-    let tamanhoSalvoCSS2 = getComputedStyle(squadAgain)
+    countdown.textContent = seconds;
+    let tamanhoSalvoCSS2 = getComputedStyle(squadAgain);
 
     if (seconds === -1) {
-      clearInterval(TimeInterval)
-      if (tamanhoSalvoCSS2.width === '1px') {
-        window.alert('Parabéns! Você é Incrível!')
+      clearInterval(TimeInterval);
+      if (tamanhoSalvoCSS2.width === "1px") {
+        window.alert("Parabéns! Você é Incrível!");
       } else {
-        window.alert('Que pena... você precisa de mais agilidade!')
+        window.alert("Que pena... você precisa de mais agilidade!");
       }
-      location.reload()
+      location.reload();
     }
-    // No if diz que se o seconds chegar a 0 irá parar a contagem.
   }
 
   function StartCountdown() {
-    TimeInterval = setInterval(UpdadeCountdown,  1000)
-    //setInterval com 1000 diz que demora 1s para diminuir a contagem do seconds.
+    TimeInterval = setInterval(UpdadeCountdown, 1000);
   }
 
   function TimerSquad() {
-    StartCountdown()
-    squadAgain.removeEventListener('mouseenter', TimerSquad)
+    StartCountdown();
+    squadAgain.removeEventListener("mouseenter", TimerSquad);
   }
-  
-  squadAgain.addEventListener('mouseenter', TimerSquad)
 
-  // Fazer a remoção de ouvinte dentro da função TimerSquad, faz com que o código seja executado normalmente com o escutador ativo até que chegue na função, fazendo com que o ouvinte e a função seja executado apenas uma única vez, impedindo com que o tempo corra mais rápido ou a função TimerSquad seja chamada mais de 1 vez com o mouseenter.
-})
+  squadAgain.addEventListener("mouseenter", TimerSquad);
+});
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Js_HTML_CSS_to-practice
-A repository that I will use to save my frequent studies in Js, HTML and CSS. Yes, I know it's too late...
+
+A repository that I will use to save my frequent studies in Js, HTML and CSS. So, there are some wrong and poor scripts, and a good code, just search.
+
+I'll make a project like a portifolium to store the mini projects.
