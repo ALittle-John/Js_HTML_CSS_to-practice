e360c938972757bd20c3602382969be9dff092de
ALittle-John <joaopsousaqi@gmail.com>
2024-09-24 14:40:54 -0300

    refactor: average calc, more objective

--- math/Average/average.js
+++
@@ -1,5 +0,0 @@
-// Arrumar o HTML: Terá 6 inputs de alunos A, B, C, D, E, F. Este arquivo js receberá os valores
-// colocados no unput, ou seja, algo dinâmico, vai juntar em uma lista e fazer o calculo comum de média,
-// e o responsável por iniciar esse calculo será uma função chamada pelo botão.
-
-function claculatin(num) {}
